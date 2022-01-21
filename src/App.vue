adf669b5ca5a1123916cbb808d884ba7e8ba9769
Deanna Sulli <dtcsulli@gmail.com>
2022-01-21 00:45:34 -0800

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,64 @@
+<script>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+import sData from './assets/data.json'
+import  MobCounter from './components/MobCounter.vue'
+export default {
+  components: {
+    MobCounter
+  },
+  data() {
+    return {
+      sData: sData
+    }
+  }
+}
+</script>
+
+<template>
+  <div class="">
+    <div class="w-full bg-purple-400 text-gray-900 ">
+      <div class="container mx-auto py-4 px-4 lg:px-0">
+        <h2 class="md:text-4xl text-2xl font-bold">
+          S Rank Spawn Kill Tracker
+        </h2>
+      </div>
+    </div>
+    <div class="container mx-auto w-full py-4 px-4 lg:px-0">
+      <div class="lg:mt-6 mt-3 pb-3" v-for="sRank in sData" :key="sRank.name">
+        <h2 class="lg:text-3xl text-2xl uppercase   tracking-widest pb-2 border-b-2 border-gray-600"><span class="text-purple-300">{{ sRank.zone }}:</span> <span class="text-pink-300">{{ sRank.name }}</span></h2>
+        <div class="grid xl:grid-cols-3 lg:grid-cols-2 md:grid-cols-1 sm:grid-cols-1 gap-4">
+        <div class="my-4 bg-gray-900 py-4 px-6 rounded-xl" v-for="guy in sRank.mobs" :key="guy.name">
+          <h3 class="font-bold text-gray-400 mb-4 text-2xl">{{ guy.name }}</h3>
+          
+          <div class="mb-6">
+            <MobCounter :mob-data="guy"/>
+          </div>
+          <div>
+            <span class="font-bold">Mobs Per Map</span>: {{ guy.available }}
+          </div>
+          <div class="mb-2">
+            <span class="font-bold">Location</span>: {{ guy.locDesc }}
+          </div>
+          <img :src="guy.mapImg" class="w-full rounded-xl" />
+          
+        </div>
+        </div>
+      </div>
+    </div>
+    <div class="w-full bg-gray-700  ">
+      <div class="container mx-auto py-4 px-4 lg:px-0 md:flex md:justify-between">
+         <div>Made by Floon (2022)</div>
+         <div >Map Images © SQUARE ENIX CO., LTD. All Rights Reserved.</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Zen+Maru+Gothic:wght@400;500;700&display=swap');
+body {
+  @apply bg-gray-800 text-gray-300;
+  font-family: 'Zen Maru Gothic', sans-serif;
+}
+</style>
---
+++ src/components/MobCounter.vue
@@ -0,0 +1,43 @@
+
+
+<template>
+  <div class="grid lg:grid-cols-2 md:grid-cols-1 sm:grid-cols-1 gap-4">
+
+    <div class="text-5xl lg:mb-0 mb-4 leading-9 lg:border-r-2 border-gray-600" :class="{'text-green-400': percentage >= 100, 'text-yellow-400': percentage >= 75 &&  percentage < 100}">
+      <div><span >{{ count }}</span> / <span>{{ mobData.req }}</span></div>
+      <span class="text-2xl text-gray-600">{{ percentage }}%</span>
+    </div>
+    <div class="lg:pl-2">
+      <input type="number" min="0" v-model="count" class="w-full">
+      <div class="mt-4 grid grid-cols-2 gap-2">
+        <button class="mark-button  text-lg tracking-wider uppercase py-1 px-4  border-t-2 rounded-full  bg-gradient-to-b text-gray-100 font-bold outline-none" @click="count++">+1</button>
+        <button class="mark-button text-lg tracking-wider uppercase py-1 px-4  border-t-2 rounded-full  bg-gradient-to-b text-gray-100 font-bold outline-none" @click="count += mobData.available">+{{ mobData.available }}</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: [
+    'mobData'
+  ],
+  data() {
+    return {
+      msg: "hi",
+      count: 0
+    }
+  },
+  computed: {
+    percentage() {
+      return Math.round(this.count / this.mobData.req * 100);
+    }
+  }
+}
+</script>
+
+<style scoped>
+a {
+  color: #42b983;
+}
+</style>
